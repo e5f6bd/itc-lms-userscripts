9faab21b3ffdc7f739c5a8f296fe0109997c8000
e5f6bd <41416412+e5f6bd@users.noreply.github.com>
2020-06-30 00:32:51 +0900

    Get and show assignments list.

--- script.user.js
+++ script.user.js
@@ -7,17 +7,74 @@
 // @match        https://itc-lms.ecc.u-tokyo.ac.jp/lms/timetable
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @resource     customCSS ./style.css
 // ==/UserScript==
-
 
 (function () {
     'use strict';
 
+    const getCourses = () => Array.from(new Set(Array.from($('.course_on_timetable')).map(e => e.id))).map(id => ({
+        id,
+        name: $(`#${id}`).text().trim()
+    }));
+
+    const parser = new DOMParser();
+    const fetchAssignments = async course => {
+        const res = await fetch(`/lms/course?idnumber=${course.id}`);
+        await new Promise(res => setTimeout(res, 500));
+        const page = parser.parseFromString(await res.text(), 'text/html');
+        document.hoge = page;
+        return [...page.querySelectorAll("#reportList .result_list_line")].map(e => {
+            const [a, , c, d] = e.querySelectorAll(".result_list_txt");
+            return {
+                title: a.innerHTML,
+                until: c.innerHTML,
+                status: d.innerHTML,
+                statusText: d.innerText.trim(),
+            }
+        });
+    }
+
+    const showAssignments = arg => {
+        const assignments = arg
+            .sort((x, y) => new Date(x.until) - new Date(y.until))
+            .filter(x => !["期限内提出", "期限後提出"].includes(x.statusText));
+        const tableBody = $("#assignmentsTableBody");
+        tableBody.children(".data").remove();
+        for (const {course, title, until, status} of assignments) {
+            $("<div>")
+                .addClass("divTableRow data")
+                .appendTo(tableBody).append([
+                $("<div>").addClass("divTableCell").append(
+                    $("<a>").attr("href", `/lms/course?idnumber=${course.id}`).text(course.name)
+                ),
+                $("<div>").addClass("divTableCell").html(title),
+                $("<div>").addClass("divTableCell").html(until),
+                $("<div>").addClass("divTableCell").html(status),
+            ]);
+        }
+    };
+
+    const reloadAssignmentsList = async () => {
+        console.log("pressed");
+        const button = $("#assignments-list-reload");
+        button.addClass("active").children("a").text("更新中");
+
+        let assignments = [];
+        for (const course of getCourses()) {
+            const newAssignments = fetchAssignments(course);
+            showAssignments(assignments);
+            (await newAssignments).forEach(e => assignments.push({...e, course}));
+        }
+        showAssignments(assignments);
+
+        button.removeClass("active").children("a").text("更新");
+    }
+
     $(() => {
-        const text = GM_getResourceText("customCSS");
-        console.log(text);
-        GM_addStyle(text);
+        GM_addStyle(GM_getResourceText("customCSS"));
         const wrapper = $("<div>")
             .attr("id", "assignments-list")
             .insertBefore("#timetable");
@@ -25,12 +82,17 @@
             .addClass("header")
             .appendTo(wrapper);
         $("<span>").addClass("icon").appendTo(header);
-        $("<h1>").text("課題一覧").appendTo(header);
+        $("<h1>").text("課題一覧").append(
+            $("<div>").attr("id", "assignments-list-reload").addClass("round_button").append(
+                $("<a>", {href: "javascript:void(0)"}).text("更新").click(reloadAssignmentsList)
+            )
+        ).appendTo(header);
 
         const table = $("<div>")
             .addClass("divTable")
             .appendTo(wrapper);
         const tableBody = $("<div>")
+            .attr("id", "assignmentsTableBody")
             .addClass("divTableBody")
             .appendTo(table);
         const headerRow = $("<div>")
